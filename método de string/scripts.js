f921a278c25765e7f5394da36c83fa11897e3198
Viniciusgigante19 <viniciusgigante935@gmail.com>
2024-05-24 18:36:00 -0300

    outro push

---
+++ método de string/scripts.js
@@ -0,0 +1,25 @@
+// length
+
+var nome = "gigante";
+
+console.log(nome.length);
+
+ var obj = "bola";
+
+ console.log(obj.length);
+
+console.log(nome[4]);
+
+var frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("rato"));
+
+//slice
+
+var rato = frase.slice(2,6)
+console.log(rato);
+
+//replace
+
+var novaFrase = frase.replace("rato","cachorro")
+console.log(novaFrase)
---
+++ método de string/scripts3.js
@@ -0,0 +1,34 @@
+//length
+
+var arr = [1,2,3,4,5];
+
+console.log(arr.length);
+
+// push
+
+arr.push(6);
+arr.push('Qualquer coisa');
+
+console.log(arr);
+
+// pop 
+
+arr.pop();
+console.log(arr);
+
+// unshift
+
+arr.unshift('teste');
+arr.unshift(0);
+
+console.log(arr);
+
+// shift
+
+arr.shift();
+
+console.log(arr);
+
+// Acessar o último elemento do array
+
+console.log(arr[arr.length - 1]);
