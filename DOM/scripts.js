b846e73e77f56ed8494928041f628729535df6d2
Viniciusgigante19 <viniciusgigante935@gmail.com>
2024-05-25 18:39:55 -0300

    push

---
+++ DOM/scripts.js
@@ -0,0 +1,25 @@
+// tag
+
+var titulo = document.getElementsByTagName('h1')[0];
+
+console.log(titulo)
+
+var lista = document.getElementsByTagName('li');
+
+console.log(lista)
+
+// id
+
+var paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+// class
+
+var itens = document.getElementsByClassName('item');
+
+console.log(itens);
+
+
+
+
